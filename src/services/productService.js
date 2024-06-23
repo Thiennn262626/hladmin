a0f7efaf1fa218d1ac41e868df3b3168dc184ed0
Qunnnn <TranVanQuan20211@gmail.com>
2024-06-23 13:07:14 +0700

    update msg

--- src/services/productService.js
+++ src/services/productService.js
@@ -43,7 +43,7 @@ async function addProduct(data) {
       data
     );
     if (response) {
-      handelException.handelNotificationSwal("Success", "success");
+      handelException.handelNotificationSwal("Thành công", "success");
       console.log("response", response);
       return response;
     }
